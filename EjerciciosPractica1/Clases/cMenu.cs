f58519d3fa44e51fa5d4c920dac2f25693c4ce83
Jerlis Medina Gomez <jerlismedina@gmail.com>
2020-07-25 10:49:57 -0400

    Diplomado Web C#, UASD. Ejercicios Practica 1, Unidad 1.

---
+++ EjerciciosPractica1/Clases/cMenu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EjerciciosPractica1.Clases;
+
+namespace EjerciciosPractica1.Clases
+{
+    public class cMenu
+    {
+        public void mostrar()
+        {
+            string opcion = "";
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("                     ==========================================================================");
+                Console.WriteLine("                                     UNIVERSIDAD AUTONOMA DE SANTO DOMINGO      ");
+                Console.WriteLine("                                                     UASD      ");
+                Console.WriteLine("                                              FACULTAD DE CIENCIAS      ");
+                Console.WriteLine("                                             ESCUELA DE INFORMATICA     ");
+
+                Console.WriteLine("                                               PRACTICA NUMERO I      ");
+                Console.WriteLine("                     ==========================================================================");
+                Console.WriteLine();
+
+
+                Console.WriteLine("**************** ELIJA UNA OPCION DEL SIGUIENTE MENU *********************");
+                Console.WriteLine("1 - Calcular Perimetro De Un Cuadrado.");
+                Console.WriteLine("2 - Calcular La Masa De Aire.");
+                Console.WriteLine("3 - Nuevo Salario De Empleado Al Aumentarle 40%.");
+                Console.WriteLine("4 - Convertir De Peso A Dolar.");
+                Console.WriteLine("5 - Porcentaje De Estudiantes Hombres Y Mujeres En El Diplomado.");
+                Console.WriteLine("S - Presione la tecla 'S' para salir");
+                Console.WriteLine("*************************************************************************");
+                Console.WriteLine();
+                Console.Write("Seleccione Una Opcion: ");
+
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        cUtilidades.PerimetroCuadrado();
+                        break;
+                    case "2":
+                        cUtilidades.CalcularMasaAire();
+                        break;
+                    case "3":
+                        cUtilidades.CalcularSalarioEmpleado();
+                        break;
+                    case "4":
+                        cUtilidades.PesoDolar();
+                        break;
+
+                    case "5":
+                        cUtilidades.PorcentajeHombreMujer();
+                        break;
+
+                    default:
+                        Console.WriteLine("<<<<<<<<<<< Hasta pronto!!! >>>>>>>>>>");
+                        opcion = "s";
+                        break;
+                }
+
+            } while (opcion != "s" & opcion != "S");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ EjerciciosPractica1/Clases/cUtilidades.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EjerciciosPractica1.Clases;
+
+namespace EjerciciosPractica1.Clases
+{
+    public class cUtilidades
+    {
+        public static void PerimetroCuadrado()
+        {
+            double lado = 0, superficie = 0, perimetro = 0;
+
+            Console.WriteLine();
+            Console.WriteLine("                     ========================== CALCULAR PERIMETRO DE UN CUADRADO ==========================");
+            Console.WriteLine();
+
+
+            Console.Write("                                  Digite el lado: ");
+            lado = double.Parse(Console.ReadLine());
+
+            superficie = Math.Round((Math.Pow(lado,2)),2);
+            perimetro = Math.Round((lado * 4),2);
+
+            Console.WriteLine();
+            Console.WriteLine($"                                 La superficie es: {superficie}");
+            Console.WriteLine($"                                  El perimetro es: {perimetro}");
+            Console.WriteLine();
+            Console.Write("                                    ===>> Presione cualquier tecla para volver al menú...");
+            Console.ReadKey();
+        }
+
+        public static void CalcularMasaAire()
+        {
+            double presion = 0, volumen = 0, temperatura = 0, masa = 0;
+            const double vValor = 0.37, vValor2 = 460;
+
+            Console.WriteLine();
+            Console.WriteLine("                     ========================== CALCULAR LA MASA DE AIRE ==========================");
+            Console.WriteLine();
+
+
+            Console.Write("                                   Presión: ");
+            presion = double.Parse(Console.ReadLine());
+
+            Console.Write("                                   Volumen: ");
+            volumen = double.Parse(Console.ReadLine());
+
+            Console.Write("                                   Temperatura: ");
+            temperatura = double.Parse(Console.ReadLine());
+
+            masa = Math.Round((presion * volumen) / (vValor * (temperatura + vValor2)),2);
+
+            Console.WriteLine();
+            Console.WriteLine($"                                 La masa es: {masa}");
+            Console.WriteLine();
+            Console.Write("                                    ===>> Presione cualquier tecla para volver al menú...");
+            Console.ReadKey();
+        }
+
+        public static void CalcularSalarioEmpleado()
+        {
+            string nombre = "", puesto = "";
+            double salAnterior = 0, salActual = 0, aumento = 0;
+            const double porcentaje = 0.40;
+
+            Console.WriteLine();
+            Console.WriteLine("                     ========================== CALCULAR NUEVO SALARIO DE EMPLEADO ==========================");
+            Console.WriteLine();
+
+
+            Console.Write("                                   Nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("                                   Puesto: ");
+            puesto = Console.ReadLine();
+
+            Console.Write("                                   Salario Anterior: ");
+            salAnterior = double.Parse(Console.ReadLine());
+
+            aumento = Math.Round((salAnterior * porcentaje),2);
+            salActual = Math.Round((salAnterior + aumento),2);
+
+            Console.WriteLine();
+            Console.WriteLine("                               Salario Anterior: {0}", salAnterior);
+            Console.WriteLine("                               Aumento 40%: {0}", aumento);
+            Console.WriteLine("                               Salario Actual: {0}", salActual);
+
+            Console.WriteLine();
+            Console.Write("                                    ===>> Presione cualquier tecla para volver al menú...");
+            Console.ReadKey();
+        }
+
+        public static void PesoDolar()
+        {
+            double peso = 0, dolar = 0, tasa = 0;
+
+            Console.WriteLine();
+            Console.WriteLine("                     ========================== CONVERTIR DE PESOS RD$ A DOLARES USD$ ===================");
+            Console.WriteLine();
+
+            Console.Write("                                   Pesos RD$: ");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.Write("                                   Tasa de cambio: ");
+            tasa = double.Parse(Console.ReadLine());
+
+            dolar = Math.Round((peso / tasa), 2);
+
+            Console.WriteLine();
+            Console.WriteLine($"                    RD$ {peso} convertido(s) a USD$ dolar(es) con una tasa de cambio de {tasa} es igual a: {dolar} USD$");
+
+            Console.WriteLine();
+            Console.Write("                                    ===>> Presione cualquier tecla para volver al menú...");
+            Console.ReadKey();
+        }
+
+        public static void PorcentajeHombreMujer()
+        {
+            double cantidadH = 0, cantidadM = 0, totalHM = 0;
+            double porcientoH = 0, porcientoM = 0;
+
+            Console.WriteLine();
+            Console.WriteLine("                     ==================== PORCENTAJE DE HOMBRES Y MUJERES EN EL DIPLOMADO C# ====================");
+            Console.WriteLine();
+
+            Console.Write("                                   Cantidad de mujeres: ");
+            cantidadM = int.Parse(Console.ReadLine());
+
+            Console.Write("                                   Cantidad de hombres: ");
+            cantidadH = int.Parse(Console.ReadLine());
+
+            totalHM = (cantidadM + cantidadH);
+            porcientoM = Math.Round(((cantidadM / totalHM) * 100),0);
+            porcientoH = Math.Round(((cantidadH / totalHM) * 100), 0);
+
+            Console.WriteLine();
+            Console.WriteLine($"                               Total estudiantes: {totalHM}");
+            Console.WriteLine($"                               El {porcientoM}% de los estudiantes son Mujeres");
+            Console.WriteLine($"                               El {porcientoH}% de los estudiantes son Hombres");
+
+            Console.WriteLine();
+            Console.Write("                                    ===>> Presione cualquier tecla para volver al menú...");
+            Console.ReadKey();
+        }
+        
+    }
+}
